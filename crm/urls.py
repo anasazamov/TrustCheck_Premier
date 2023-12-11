1e240e9514ffec1b25228ed92781ca330bee5d9e
anasazamov <anasazamov55@gmail.com>
2023-12-12 02:47:08 +0500

    fix urls

--- crm/urls.py
+++ crm/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateProduct, UtilizedProduct, CreateProductTable
+from .views import CreateProduct, UtilizedProduct, CreateProductTable, GetAllUser
 
 urlpatterns = [
     path('create-product/', CreateProduct.as_view(), name='create-product'),
     path('create-product/<int:pk>/', CreateProduct.as_view(), name='get-update-delete-product'),
     path('utilized-product/', UtilizedProduct.as_view(), name='get-utilized-product'),
     path('create-product-table/', CreateProductTable.as_view(), name='get-create-product-table'),
+    path('get-all-user/', GetAllUser.as_view(), name='get-all-user-table'),
 ]
