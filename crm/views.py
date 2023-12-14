d8fd146372cfeac5dd6e3e8adb76f9e4283ae849
anasazamov <anasazamov55@gmail.com>
2023-12-14 14:20:44 +0500

    Optimezed post function

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,4 @@
+from msilib import datasizemask
 from rest_framework import status
 from rest_framework.request import Request 
 from rest_framework.response import Response
@@ -5,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
+from django.db import transaction
 from userverification.serializer import UserSerializer
 from django.utils import timezone
 from .serializer import *
@@ -32,36 +34,47 @@ class CreateProductAPI(APIView):
             serializer = ProductSerializer(products,many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         
-    def post(self,request: Request):
-
+    @transaction.atomic
+    def post(self, request: Request):
         try:
-            user = request.user
+            user: User = request.user
             data = request.data
         except:
-            return Response({"message":"bad request"},status=status.HTTP_400_BAD_REQUEST)
-        
+            return Response({"message": "bad request"}, status=status.HTTP_400_BAD_REQUEST)
+
         name = data.get("name")
         price = data.get("price")
         description = data.get("description")
         end_date = data.get("end_date")
         how_many = data.get("how_many")
         created_products = []
-        create_for_CreateProduct = []
-        
-        for i in range(0,how_many):
-            product_seria_num = sha256_hash(f"{int(timezone.now().timestamp())}".encode('utf-8'))
-            product = Product(name=name,price=price,description=description,product_seria_num=product_seria_num,end_date=end_date)
-            product.save(force_insert=True)
-            create_at = CreateProduct(user=user,product=product)
-            create_at.save(force_insert=True)
-            created_products.append(product)
-            create_for_CreateProduct.append(create_at)
 
-       
+        # Generate serial numbers for all products outside the loop
+        product_serial_numbers = [sha256_hash(str(i).encode('utf-8')) for i in range(Product.objects.count() + 1, Product.objects.count() + how_many + 1)]
 
-        serializer = ProductSerializer(created_products,many=True)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+        with transaction.atomic():
+            products_to_create = [
+                Product(
+                    name=name,
+                    price=price,
+                    description=description,
+                    product_seria_num=serial_number,
+                    end_date=end_date
+                )
+                for serial_number in product_serial_numbers
+            ]
+
+            created_products = Product.objects.bulk_create(products_to_create)
+
+            create_for_CreateProduct = [
+                CreateProduct(user=user, product=product)
+                for product in created_products
+            ]
+
+            CreateProduct.objects.bulk_create(create_for_CreateProduct)
+
+        serializer = ProductSerializer(created_products, many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)    
     def delete(self,request: Request,pk=False):
 
         if pk:
